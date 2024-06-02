77a13c8b2c35ce00ca53eeaef28143367525cc22
ameen <mohammedameen9011@gmail.com>
2024-06-03 01:17:49 +0530

    logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,35 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import favicons from "astro-favicons";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://ameenshaikh.com",
-  integrations: [mdx(), sitemap(), tailwind()],
-  
+  integrations: [mdx(), sitemap(), tailwind(), icon(),
+  favicons({
+    // masterPicture: "./src/favicon.svg",
+    // emitAssets: true,
+
+    // You should adjust the following options accordingly
+    appName: "",
+    appShortName: "",
+    appDescription: "",
+    // dir:"auto",
+    lang: "en-US",
+    // display: "standalone",
+    // orientation: "any",
+    // start_url: "/?homescreen=1",
+    background: "#fff",
+    theme_color: "#fff",
+
+    faviconsDarkMode: false, // default `true`, Make favicon compatible with light and dark modes
+
+    // appleStatusBarStyle: "black-translucent",
+
+    //....
+  }),
+
+],
 });
